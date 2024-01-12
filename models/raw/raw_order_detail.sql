90d272bcc5725bac31a4b6de537941d7edf3da69
Hannah Lim <s10243399@connect.np.edu.sg>
2024-01-12 08:22:15 +0000

    added incremental model

--- models/raw/raw_order_detail.sql
+++ models/raw/raw_order_detail.sql
@@ -1,5 +1,13 @@
-{{ config (materialized='table')}}
+--{{ config (materialized='table')}}
 
-Select *
-From 
-NWT_RAW_DATABASE.NWT.ORDER_DETAIL
+--Select *
+--From 
+--NWT_RAW_DATABASE.NWT.ORDER_DETAIL
+
+{{ config(materialized='incremental', unique_key='ORDERID') }}
+
+SELECT *
+FROM NWT_RAW_DATABASE.NWT.ORDER_DETAIL_FRESH
+{% if is_incremental() %}
+WHERE CAST(ORDERID AS BIGINT) > (SELECT MAX(CAST(ORDERID AS BIGINT))  FROM {{this}})
+{% endif %}
